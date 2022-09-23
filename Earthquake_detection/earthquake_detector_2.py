c3a89f55ef80fe348448a94213527672d3bcd950
ndesh02 <54901274+ndesh02@users.noreply.github.com>
2022-09-23 11:58:24 -0400

    finished earthquake detector

--- Earthquake_detection/earthquake_detector_2.py
+++ Earthquake_detection/earthquake_detector_2.py
@@ -3,6 +3,7 @@ import numpy as np
 
 class EarthquakeDetector:
     old_seismograph_roc;
+    new_datapoints;
     def __init__(self,
                 seismograph_count, 
                 sample_rate_hz,
@@ -15,27 +16,18 @@ class EarthquakeDetector:
         3. Set a flag (called possibly_earthquake) to true
         4. If all the detectors have set their flags to true, trigger the alert
         """
+
+        threshold = 5
+
         if old_seismograph_roc == NULL:
             old_seismograph_roc = np.full((seismograph_count, 10), None)
 
-        setpoints = False
-        new_seismograph_roc = None
+        if new_datapoints == NULL:
+            pass
 
-        """
-        This threshold should be changed based on how often we want the earthquake detector to provide alerts - should be done with testing.
-        I have set it to 3 temporarily.
-        """
-        threshold = 3 
         possibly_earthquake = np.full(seismograph_count, False)
-        """
-        Args: 
-            seismograph_count: the number of seismographs in the detector network.
-            sample_rate_hz: the sample rate of the seismograph.
-            alert_callback: a callback function if an earthquake is detected.
-        """
-
+        
         for i in range(seismograph_count):
-            new_datapoints = new_samples(i)
             old_seismograph_roc[i][0] = (new_datapoints[1] - new_datapoints[0])/sample_rate_hz
             for j in range(new_datapoints)-1:
                 old_seismograph_roc[i][j] = new_datapoints[j] - new_datapoints[j-1] / sample_rate_hz
@@ -49,10 +41,17 @@ class EarthquakeDetector:
         if possibly_earthquake.all() == True:
             alert_callback()
 
+        """
+        Args: 
+            seismograph_count: the number of seismographs in the detector network.
+            sample_rate_hz: the sample rate of the seismograph.
+            alert_callback: a callback function if an earthquake is detected.
+        """
+
+
         pass
 
     def new_samples(self, seismograph_id, samples):
-        """I have made an assumption that calling this function provides me with the samples I need for my seismograph"""
 
         """New samples are available for a seismograph.
 
@@ -60,4 +59,12 @@ class EarthquakeDetector:
             seismograph_id: the zero-indexed seismograph id.
             samples: an ndarray of integer samples.
         """
+        if new_datapoints == NULL:
+            new_datapoints = {}
+        
+        if new_datapoints.get(seismograph_id) == None:
+            new_datapoints[seismograph_id] = samples
+        else:
+            new_datapoints[seismograph_id].append()
+
         pass
